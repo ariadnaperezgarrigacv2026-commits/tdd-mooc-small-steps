263b3c2c0c5ae28c531048208c7f45eb63f06067
Ariadna Perez <aperezgarrig@ukaachen.de>
2026-02-26 13:03:25 +0100

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -77,7 +77,7 @@ function createApp(database: Database) {
     return reduction;
   }
 
-  function isMonday(date: Date) {
+  function isMonday(date: Temporal.PlainDate) {
     return date.getUTCDay() === 1;
   }
 
